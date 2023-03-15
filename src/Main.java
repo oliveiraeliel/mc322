3cca4796a2b3af5f15b83e0467c54086cc314e1a
Eliel Oliveira Da Silva <e221437@dac.unicamp.br>
2023-03-14 21:50:03 -0300

    first init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("asdfsad");
+		Cliente cliente = new Cliente("null", "515", "null", 0, "null");
+
+		System.out.println(cliente.getCpf());
+	}
+
+}
