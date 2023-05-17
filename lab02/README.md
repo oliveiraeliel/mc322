e6413ba14facbe30ed8797878813375c4de4287a
Eliel Oliveira Da Silva <e221437@dac.unicamp.br>
2023-05-16 22:01:04 -0300

    arrumando as pastas

--- lab02/README.md
+++
@@ -1,2 +0,0 @@
-# mc322_1s2023
-Projeto da disciplina MC322 (Programação Orientada a Objetos) turma C - Unicamp
---
+++ src/execeptions/FrotaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package execeptions;
+
+public class FrotaNaoEncontradaException extends Exception{
+    public FrotaNaoEncontradaException(String message){
+        super(message);
+    }
+}
