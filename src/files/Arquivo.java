b0f9c34bb197c9e28a69ae80d221945b58bb85db
Eliel Oliveira Da Silva <e221437@dac.unicamp.br>
2023-06-06 22:45:49 -0300

    ....

---
+++ src/files/Arquivo.java
@@ -0,0 +1,8 @@
+package files;
+
+import entidades.Cliente.Cliente;
+
+public class Arquivo {
+    public static 
+    public static Cliente lerCliente()
+}
---
+++ src/interfaces/I_Arquivo.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public interface I_Arquivo {
+    public boolean gravarArquivo();
+    public String lerArquivo();
+}
